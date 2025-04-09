22325e14c43fa611d97eee8edc579f5ea44e02ee
edmondb3933 <edmondb3933@student.faytechcc.edu>
2025-04-09 12:59:31 -0400

    Add files via upload
    First Submission

---
+++ P1LAB1_EdmondBrandon.py
@@ -0,0 +1,7 @@
+# Brandon Edmond
+# 4/9/2025
+# P1LAB1
+# Get user input
+firstname = input('Enter your first name: ')
+lastname = input('Enter your last name: ')
+print("Hello,",firstname, lastname+"!", "Welcome to CTI-110")
